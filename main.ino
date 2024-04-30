3a772bb923d99e4993859c9070b956ef315caf86
John-Althoff <123976390+1103-Althoff-John@users.noreply.github.com>
2024-04-30 08:54:47 -0700

    Create main and setup functions

---
+++ main.ino
@@ -0,0 +1,14 @@
+/*
+ *  Names: Jackson Robertson, Timmy Norris, John Althoff
+ *  Assignment: Final Project 
+ *  Class: CPE301 Spring 2023
+ *  Date: 4/30/2024
+ */
+
+void setup() {
+
+}
+
+void loop() {
+
+}
